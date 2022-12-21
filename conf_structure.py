7a7c344c63574ec72e23eb28c353a81ca567bc9c
Jassiel Hernandez <jassiel.hernandez@kiubix.com>
2022-12-20 23:50:27 -0600

    Readme y structure py

---
+++ conf_structure.py
@@ -0,0 +1,8 @@
+# ais.usvisa-info.com credentials
+username = 'username of the account'
+password = 'password of the account'
+url_id = 'XXXXXXXX' # This is the number id in the url (e.g.: https://ais.usvisa-info.com/en-pe/niv/schedule/xxxxxxxx)
+country_code = 'mx' # This is the country code in the url
+city_id = {66: 'Guadalajara', 71: 'Monterrey'} #Guadalajara, Monterrey
+
+## Telegram Bot things
